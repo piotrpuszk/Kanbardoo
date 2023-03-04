f62861b9832aeea4a2dcdf7d260b61119fb9f018
Piotr Puszkarski <piotrpuszk@gmail.com>
2023-03-04 17:26:58 +0100

    wwwroot removed

--- Kanbardoo.WebAPI/Controllers/FallbackController.cs
+++ Kanbardoo.WebAPI/Controllers/FallbackController.cs
@@ -7,7 +7,7 @@ public class FallbackController : Controller
     public ActionResult Index()
     {
         return PhysicalFile(
-            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"),
+            Path.Combine(Directory.GetCurrentDirectory(), "index.html"),
             "text/HTML");
     }
 }
